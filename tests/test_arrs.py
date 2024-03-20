b8c5b0c2783b8297af427af19f98693f54500aa4
Maria Lomets <marusya.lomets@gmail.com>
2024-03-21 00:56:28 +0300

    test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,17 @@
+import unittest
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3, 4], 2, "test") == 3
     assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 8) == [3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 10) == [3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 0, 2) == [1, 2]
+
